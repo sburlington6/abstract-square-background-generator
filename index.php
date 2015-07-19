33ab8fa7e5bb5c1fa8ac3eea8935e40d88ab9b7f
Richard Bird <sburlington6@yahoo.com>
2015-07-18 21:09:57 -0400

    Init
    added the initial php and css files

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+?>
+
+<!doctype html>
+
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+
+		<title>abstract-square-background-generator</title>
+		<meta name="description" content="abstract-square-background-generator">
+		<meta name="author" content="Richard Bird">
+
+		<link rel="stylesheet" href="style.css">
+
+		<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	</head>
+
+	<body>
+		<script src="js/scripts.js"></script>
+	</body>
+</html>
